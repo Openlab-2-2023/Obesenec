41e7315673b1c03f1a0b21211f08af48cc66152c
Pavol Zuščák <palkozuscak2@gmail.com>
2024-05-28 08:43:36 +0200

    palo js

---
+++ script.js
@@ -0,0 +1,18 @@
+
+const options = {
+    aroma: " Pleasing smell",
+    pepper: "Salt's partner",
+    halt: "put a stop to",
+    jump: "Rise suddenly",
+    shuffle: "Mix cards up",
+    combine: "Add; Mix",
+    chaos: "Total disorder",
+    labyrinth: "Maze",
+    disturb: "Interrupt; upset",
+    shift: "Move; Period of word",
+    machine: "Device or appliance",
+};
+const message = document.getElemntByld("message");
+const hintRef = document.querySelector(".hint-ref");
+const controls = document.querySelector(".controls-container");
+const startBtn = document.getElementById("start");
